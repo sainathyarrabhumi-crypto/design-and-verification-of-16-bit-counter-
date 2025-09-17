5d13e474a3bec8782974dbb7d5abf93404fd7bea
yerrabhumi sainath reddy <sainathyarrabhumi@gmail.com>
2025-09-17 15:49:04 +0530

    Add files via upload

---
+++ count_package.sv
@@ -0,0 +1,12 @@
+package count_package;
+int no_of_transactions= 2;
+`include "count_trans.sv"
+`include "count_gen.sv"
+`include "count_wr.sv"
+`include "count_wr_mon.sv"
+`include "count_ref_model.sv"
+`include "count_rd_mon.sv"
+`include "count_sb.sv"
+`include "count_env.sv"
+`include "test.sv"
+endpackage
---
+++ count_top.sv
@@ -0,0 +1,24 @@
+class count_top;
+import count_package::*;
+reg clk;
+count_in duv_if(clk):
+test test_h;
+count_16 duv(.clk(clk),.load(duv_if.load),.mode(duv_if.mode),.data_in(duv_if.data_in),.reset(duv_if.reset),.data_out(duv_if.data_out));
+initial
+begin
+clk=1'b0;
+forever
+#10 clk=~clk;
+end
+initial
+begin
+if($test$plusargs("TEST1"))
+begin
+test_h(duv_if,duv_if,duv_if);
+no_of_transactions=2;
+test_h.build();
+test_h.run();
+end
+$finish;
+end
+endclass
